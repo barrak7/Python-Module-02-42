3df390f1d94e3213cb2f7f2b775cbea69550c419
Hamza Barrak <hbarrak@e2r13p9.1337.ma>
2023-02-01 19:06:53 +0100

    Success

--- ex05/TinyStatistician.py
+++ ex05/TinyStatistician.py
@@ -1,12 +1,16 @@
 #!/bin/python3
 
+import math
+
 class TinyStatistician:
 	@staticmethod
 	def mean(x):
 		if not x:
 			return
+		if not isinstance(x, list) or not all([isinstance(e, (int, float)) for e in x]):
+			return
 		m = len(x)
-		if not isinstance(x, list) or m == 0 or not all([isinstance(e, (int, float)) for e in x]):
+		if m == 0:
 			return
 		value = 0
 		for element in x:
@@ -18,8 +22,10 @@ class TinyStatistician:
 	def median(x):
 		if not x:
 			return
+		if not isinstance(x, list) or not all([isinstance(e, (int, float)) for e in x]):
+			return
 		m = len(x)
-		if not isinstance(x, list) or m == 0 or not all([isinstance(e, (int, float)) for e in x]):
+		if m == 0:
 			return
 		x = sorted(x)
 		if m % 2 == 0:
@@ -33,26 +39,45 @@ class TinyStatistician:
 	def quartile(x):
 		if not x:
 			return
+		if not isinstance(x, list) or not all([isinstance(e, (int, float)) for e in x]):
+			return
 		m = len(x)
-		if not isinstance(x, list) or m == 0 or not all([isinstance(e, (int, float)) for e in x]):
+		if m == 0:
 			return
 		x.sort()
-		values = [TinyStatistician.median(x[:int(m/2)]), TinyStatistician.median(x[int(m/2):])]
+		if m % 2 == 0:
+			values = [TinyStatistician.median(x[:m//2]), TinyStatistician.median(x[m//2:])]
+		else:
+			values = [TinyStatistician.median(x[:m//2 + 1]), TinyStatistician.median(x[m//2:])]
 		return values
 
 	@staticmethod
 	def var(x):
 		if not x:
 			return
+		if not isinstance(x, list) or not all([isinstance(e, (int, float)) for e in x]):
+			return
 		m = len(x)
-		if not isinstance(x, list) or m == 0 or not all([isinstance(e, (int, float)) for e in x]):
+		if m == 0:
 			return
 		x.sort()
-		med = TinyStatistician.median(x)
+		med = TinyStatistician.mean(x)
 		value = 0
 		for element in x:
-			value += (element - m) * (element - m)
-		return float(value / 2)
+			value += (element - med) ** 2
+		return float(value / m)
+
+	@staticmethod
+	def std(x):
+		if not x:
+			return
+		if not isinstance(x, list) or not all([isinstance(e, (int, float)) for e in x]):
+			return
+		m = len(x)
+		if m == 0:
+			return
+		value = TinyStatistician.var(x)
+		return math.sqrt(value)
 
 tstat = TinyStatistician()
 a = [1, 42, 300, 10, 59]
@@ -63,4 +88,6 @@ print(tstat.median(a))
 print(tstat.quartile(a))
 # Expected result: [10.0, 59.0]
 print(tstat.var(a))
-
+# Expected result: 12279.439999999999
+print(tstat.std(a))
+# Expected result: 110.81263465868862
